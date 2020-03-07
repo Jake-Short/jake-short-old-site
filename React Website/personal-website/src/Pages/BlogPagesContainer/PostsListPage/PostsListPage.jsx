a474707089ba03bcd7504c092bd36b79d88e99a7
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2020-03-07 12:18:42 -0800

    Added some minor things

--- React Website/personal-website/src/Pages/BlogPagesContainer/PostsListPage/PostsListPage.jsx
+++ React Website/personal-website/src/Pages/BlogPagesContainer/PostsListPage/PostsListPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './PostsListPage.module.css';
+import Highlight from 'react-highlighter';
 
 // Import components
 import NavigationBar from '../../../Components/NavigationBar/NavigationBar';
@@ -9,10 +10,21 @@ import NavigationBar from '../../../Components/NavigationBar/NavigationBar';
 import { FeaturedItems } from '../BlogPosts/PostsData';
 
 export default class PostsListPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchBarValue: '',
+        }
+    }
+
+    updateSearch(e) {
+        this.setState({ searchBarValue: e.target.value });
+    }
+
     render() {
         var featuredItems = FeaturedItems.map((item, index) => {
             return (
-                <Link to={'/blog/post/' + item.postFileName} style={{ textDecoration: 'none' }}>
+                <Link key={index} to={'/blog/post/' + item.postFileName} style={{ textDecoration: 'none' }}>
                     <div className={styles.postCard}>
                         <img src={require('../BlogThumbnails/' + item.thumbnailFileName)} className={styles.postCardThumbnail} alt="Thumbnail" />
 
@@ -23,6 +35,28 @@ export default class PostsListPage extends React.Component {
             )
         });
 
+        var filteredItemsArray = FeaturedItems.filter((item) => {
+            if(item.title.toLowerCase().includes(this.state.searchBarValue.toLowerCase()) || item.tags.find(a => a.toLowerCase().includes(this.state.searchBarValue.toLowerCase()) )) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        })
+
+        var filteredItems = filteredItemsArray.map((item, index) => {
+            return (
+                <Link key={index} to={'/blog/post/' + item.postFileName} className={styles.postCardLink}>
+                    <div className={styles.postCard}>
+                        <img src={require('../BlogThumbnails/' + item.thumbnailFileName)} className={styles.postCardThumbnail} alt="Thumbnail" />
+
+                        <Highlight className={styles.postCardTitle} matchElement="mark" matchStyle={{ background: 'rgba(255, 255, 255, 0.2)', color: '#ffffff' }} search={this.state.searchBarValue}>{item.title}</Highlight>
+                        <p className={styles.postCardDescription}>{item.description}</p>
+                    </div>
+                </Link>
+            )
+        })
+
         return (
             <React.Fragment>
                 <div className={styles.mainContentContainer}>
@@ -30,9 +64,12 @@ export default class PostsListPage extends React.Component {
 
                     {/** Show all posts here */}
                     <div className={styles.postCardsContainer}>
+                        <input type="text" placeholder="Search blog posts..." className={styles.searchBar} value={this.state.searchBarValue} onChange={(e) => this.updateSearch(e)} />
+
                         <div className={styles.postCardsContainerInner}>
 
-                            {featuredItems}
+                            {this.state.searchBarValue.length > 0 ? filteredItems : featuredItems}
+                            {this.state.searchBarValue.length > 0 && filteredItemsArray.length < 1 ? <p className={styles.noResultsText}>No results for '{this.state.searchBarValue}'.</p> : null}
 
                         </div>
                     </div>
